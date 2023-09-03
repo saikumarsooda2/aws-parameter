18e3b5a42873ef8664f0c01968656063f8e21e83
saikumarsooda2 <101648703+saikumarsooda2@users.noreply.github.com>
2023-09-03 15:45:10 +0100

    added dispatch payment

--- main.tf
+++ main.tf
@@ -1,8 +1,8 @@
 resource "aws_ssm_parameter" "params" {
   count = length(var.params)
-  name        = var.params[count.index].name
-  type        = var.params[count.index].type
-  value       = var.params[count.index].value
+  name  = var.params[count.index].name
+  type  = var.params[count.index].type
+  value = var.params[count.index].value
 }
 
 variable "params" {
--- state.tf
+++ state.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket = "terraform-practice12"
-    key    = "sample/terraform.tfstate"
+    key    = "params/terraform.tfstate"
     region = "us-east-1"
   }
 }
