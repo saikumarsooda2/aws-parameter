70d8e8e9fbc1edd2517b5f68381dab63931f4cbd
saikumarsooda2 <101648703+saikumarsooda2@users.noreply.github.com>
2023-09-03 15:51:16 +0100

    added dispatch payment

--- main.tf
+++ main.tf
@@ -7,10 +7,10 @@ resource "aws_ssm_parameter" "params" {
 
 variable "params" {
   default = [
-    { name = "roboshop.dev.frontend.catalogue_url", value = "http://catalogue-dev.devopspractice.lol", type = "string" },
-    { name = "roboshop.dev.frontend.user_url", value = "http://user-dev.devopspractice.lol", type = "string" },
-    { name = "roboshop.dev.frontend.cart_url", value = "http://cart-dev.devopspractice.lol", type = "string" },
-    { name = "roboshop.dev.frontend.shipping_url", value = "http://shipping-dev.devopspractice.lol", type = "string" } ,
-    { name = "roboshop.dev.frontend.payment_url", value = "http://payment-dev.devopspractice.lol", type = "string" }
+    { name = "roboshop.dev.frontend.catalogue_url", value = "http://catalogue-dev.devopspractice.lol", type = "String" },
+    { name = "roboshop.dev.frontend.user_url", value = "http://user-dev.devopspractice.lol", type = "String" },
+    { name = "roboshop.dev.frontend.cart_url", value = "http://cart-dev.devopspractice.lol", type = "String" },
+    { name = "roboshop.dev.frontend.shipping_url", value = "http://shipping-dev.devopspractice.lol", type = "String" } ,
+    { name = "roboshop.dev.frontend.payment_url", value = "http://payment-dev.devopspractice.lol", type = "String" }
   ]
 }
