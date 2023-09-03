61cb8bba2ec7d933ecaba32d06284a7bd8c08141
saikumarsooda2 <101648703+saikumarsooda2@users.noreply.github.com>
2023-09-03 14:54:00 +0100

    added dispatch payment

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 resource "aws_ssm_parameter" "params" {
-  count = lenth(var.params)
+  count = length(var.params)
   name        = var.params[count.index].name
   type        = var.params[count.index].type
   value       = var.params[count.index].value
