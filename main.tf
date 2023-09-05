7a2e198f7130a99dc444623bfc8fec933034e152
saikumarsooda2 <101648703+saikumarsooda2@users.noreply.github.com>
2023-09-05 20:29:16 +0100

    added backend

--- main.tf
+++ main.tf
@@ -11,6 +11,20 @@ variable "params" {
     { name = "roboshop.dev.frontend.user_url", value = "http://user-dev.devopspractice.lol:8080", type = "String" },
     { name = "roboshop.dev.frontend.cart_url", value = "http://cart-dev.devopspractice.lol:8080", type = "String" },
     { name = "roboshop.dev.frontend.shipping_url", value = "http://shipping-dev.devopspractice.lol:8080", type = "String" } ,
-    { name = "roboshop.dev.frontend.payment_url", value = "http://payment-dev.devopspractice.lol:8080", type = "String" }
+    { name = "roboshop.dev.frontend.payment_url", value = "http://payment-dev.devopspractice.lol:8080", type = "String" },
+    { name = "roboshop.dev.catalogue.mongo_url", value = "mongodb://mongodb-dev.devopspractice.lol:27017/catalogue", type = "String" },
+    { name = "roboshop.dev.catalogue.mongo", value = "true", type = "String" },
+    { name = "roboshop.dev.dispatch.rabbitmq_host", value = "rabbitmq-dev.devopspractice.lol", type = "String" },
+    { name = "roboshop.dev.payment.cart_host", value = "<cart-dev.devopspractice.lol>", type = "String" },
+    { name = "roboshop.dev.payment.cart_port", value = "8080", type = "SecureString" },
+    { name = "roboshop.dev.payment.user_host", value = "<user-dev.devopspractice.lol>", type = "String" },
+    { name = "roboshop.dev.payment.user_port", value = "8080", type = "String" },
+    { name = "roboshop.dev.payment.amqp_host", value = "<rabbitmq-dev.devopspractice.lol>", type = "String" },
+    { name = "roboshop.dev.payment.amqp_user", value = "roboshop", type = "String" },
+    { name = "roboshop.dev.payment.amqp_pass", value = "roboshop123", type = "SecureString" },
+    { name = "roboshop.dev.shipping.cart.end_point", value = "<cart-dev.devopspractice.lol>:8080", type = "String" },
+    { name = "roboshop.dev.shipping.db_host", value = "<mysql-dev.devopspractice.lol>", type = "String" }
+
+
   ]
 }
